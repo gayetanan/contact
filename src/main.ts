e5b52fdd890322437b54c0b343140971f7a4baaf
gayetanan <gayetan.an@gmail.com>
2024-06-02 20:13:35 -0400

    filter list func

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-import "./css/style.css"
+import "./css/style.css";
+const filterButton = document.querySelector(".filter-trigger") as HTMLButtonElement;
+const categotyListButton = document.querySelectorAll("#category-list li") as NodeList;
+
+
+const toggleFilterList = function (e: Event): void {
+    const button = e.target as HTMLElement;
+    const id: string | undefined = button.dataset.target;
+
+    const categotyList: HTMLElement | null = document.querySelector(`#${id}`);
+    if (categotyList?.getAttribute("aria-expanded") === "true") {
+        categotyList.setAttribute("aria-expanded", "false");
+    }
+    else {
+        categotyList?.setAttribute("aria-expanded", "true");
+    }
+}
+
+categotyListButton.forEach((button: Node) => {
+    button.addEventListener("click", (e: Event) => {
+        const element = <HTMLLIElement>e.target;
+        console.log(element.dataset.category)
+
+    })
+})
+
+
+
+filterButton?.addEventListener("click", toggleFilterList);
