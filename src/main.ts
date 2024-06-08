a0cffb8c231da8b4b28662d84b5a052aac305aa0
gayetanan <gayetan.an@gmail.com>
2024-06-08 09:41:42 -0400

    reset fields

--- src/main.ts
+++ src/main.ts
@@ -2,57 +2,101 @@ import "./css/style.css";
 const createButton = document.querySelector(".c-btn");
 const closeButton = document.querySelector(".cl-btn");
 const contactForm = document.querySelector("#c-form");
+
+const CONTACTS: Contact[] = [];
 // Open BOX MODEL FOR CREATING Contact
 const boxToggler = (state: string, trigger: HTMLElement): void => {
-    const boxID = trigger.dataset.target!;
-    const boxModel = document.querySelector(boxID);
-    boxModel?.setAttribute("aria-hidden", state);
+  const boxID = trigger.dataset.target!;
+  const boxModel = document.querySelector(boxID);
+  boxModel?.setAttribute("aria-hidden", state);
 };
 createButton?.addEventListener("click", (e) => {
-    const button = <HTMLElement>e.target;
-    boxToggler("false", button)
+  const button = <HTMLElement>e.target;
+  boxToggler("false", button)
 });
 
 closeButton?.addEventListener("click", (e) => {
-    const button = <HTMLElement>e.target;
-    boxToggler("true", button)
+  const button = <HTMLElement>e.target;
+  boxToggler("true", button)
 });
 
+type Contact = {
+  id: string,
+  firstname: string,
+  lastname?: string,
+  cover?: string
+  category: string,
+  phone: string
+}
 
+const contactUIComponent = (contact: Contact): string => {
+  return `
+    <li class="flex justify-between items-center relative" data-id="${contact.id}">
+         <div class="flex items-center gap-4">
+           <div class="w-10 rounded-full overflow-hidden">
+             <img src="${contact.cover}">
+           </div>
+           <div class="grid gap-[2px]">
+             <span class="text-base font-bold">${contact.firstname} ${contact.lastname ?? ""}</span>
+             <span aria-${contact.category}
+               class="w-fit text-[10px] py-[2px] px-2 aria-[${contact.category}]:bg-yellow-300 rounded-md">Family</span>
+           </div>
+         </div>
+         <div>
+           <button type="button" class="contact-menu bg-gray-200 inline-flex p-1 rounded-full">
+             <i class='bx bx-dots-vertical-rounded'></i>
+           </button>
+         </div>
+
+       </li>`
+}
+
+// Displaying contact to UI
+const addContactToUi = (contacts: Contact[]): void => {
+  const contactList: Element = document.querySelector(".contacts")!;
+  const contactData = contacts.map((conatct) => {
+    return contactUIComponent(conatct);
+  });
+  contactList.innerHTML = contactData.join(" ");
+}
+const resetFields = (fieldList: HTMLInputElement[]) => {
+  fieldList.forEach((field) => {
+    field.value = ""
+  })
+}
 contactForm?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const form = <HTMLFormElement>e.target;
-    const fieldsCollection = form.elements;
-    // Ignoring case there is multiple input with the same name;
-    const firstname = fieldsCollection.namedItem("firstname") as HTMLInputElement;
-    const lastname = fieldsCollection.namedItem("lastname") as HTMLInputElement;
-    const phone = fieldsCollection.namedItem("phone") as HTMLInputElement;
-    const category = fieldsCollection.namedItem("category") as HTMLInputElement;
-    //
-    const phonePattern = RegExp("[0-9]{3}-[0-9]{3}-[0-9]{4}")
+  e.preventDefault();
+  const form = <HTMLFormElement>e.target;
+  const fieldsCollection = form.elements;
+  // Ignoring case there is multiple input with the same name;
+  const firstname = fieldsCollection.namedItem("firstname") as HTMLInputElement;
+  const lastname = fieldsCollection.namedItem("lastname") as HTMLInputElement;
+  const phone = fieldsCollection.namedItem("phone") as HTMLInputElement;
+  let category = fieldsCollection.namedItem("category") as HTMLInputElement;
+  if (firstname.value.trim() === "" && lastname.value.trim() === "") {
+    console.log("");
+    return;
+  };
+  if (phone.value.trim() === "") {
+    console.log("phone number is required");
+    return;
+  };
+  let actualCategory = category.value || "friend";
+  const id = new Date().getTime()
 
-    if (firstname.value.trim() === "" && lastname.value.trim() === "") {
-        console.log("not valid");
-        return;
-    }
+  const newContact: Contact = {
+    id: `@${id}`,
+    firstname: firstname.value,
+    lastname: lastname.value,
+    category: actualCategory,
+    phone: phone.value
+  };
+  CONTACTS.push(newContact);
+  addContactToUi(CONTACTS);
+  resetFields([firstname, lastname, phone]);
 })
 
 
-
-
-
-// type Contact = {
-//     id: string,
-//     firstname: string,
-//     lastname: string,
-//     cover: string
-//     email: string,
-//     category: string,
-//     phones: {
-//         personnal: string,
-//         company?: string
-//     };
-// }
 
 // // type ContactMenu = {
 // //     position: {
@@ -64,7 +108,7 @@ contactForm?.addEventListener("submit", (e) => {
 // //         element: HTMLElement | null
 // //     }
 // // }
-// // const CONTACTS: Contact[] = datas
+
 
 // // const contactMenu: ContactMenu = {
 // //     position: { top: 0, right: 0 },
@@ -73,35 +117,7 @@ contactForm?.addEventListener("submit", (e) => {
 // //         element: null
 // //     }
 // // }
-// // //Displaying contact to UI
-// // const addContactToUi = (contacts: Contact[]): void => {
-// //     const contactList: Element = document.querySelector(".contacts")!;
 
-// //     const contactsData = contacts.map((conatct) => {
-// //         return `
-// //         <li class="flex justify-between items-center relative" data-id="${conatct.id}">
-// //         <div class="flex items-center gap-4">
-// //           <div class="w-10 rounded-full overflow-hidden">
-// //             <img src="${conatct.cover}">
-// //           </div>
-// //           <div class="grid gap-[2px]">
-// //             <span class="text-base font-bold">${conatct.firstname} ${conatct.lastname}</span>
-// //             <span aria-${conatct.category}
-// //               class="w-fit text-[10px] py-[2px] px-2 aria-[${conatct.category}]:bg-yellow-300 rounded-md">Family</span>
-// //           </div>
-// //         </div>
-// //         <div>
-// //           <button type="button" class="contact-menu bg-gray-200 inline-flex p-1 rounded-full">
-// //             <i class='bx bx-dots-vertical-rounded'></i>
-// //           </button>
-// //         </div>
-
-// //       </li>
-// //         `
-// //     })
-
-// //     contactList.innerHTML = contactsData.join(" ");
-// // }
 // // const resetActiveElement = () => {
 // //     contactMenu.activeElement.id = null
 // //     contactMenu.activeElement.element = null
